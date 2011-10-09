a0c9816862a88c344cde6e8f08b28a1315c26842
Gesiel Rios <gesielrios@gmail.com>
2011-10-08 23:51:45 -0300

    Comite inicial da app de geracao de certificados de participacao do VII GURUCE.

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.10'
+gem 'prawn'
+gem 'jquery-rails'
+gem 'sqlite3'
+
+group :development, :test do
+	gem 'capistrano'
+	gem "passenger"
+	gem "autotest"
+	gem "autotest-growl"
+	gem "rspec", "2.5.0"
+	gem "rspec-rails", "2.5.0"
+	gem 'simplecov', '0.4.2', :require => false
+	gem "spork"
+	gem "launchy"
+	gem "ruby-debug19"
+	gem "pdf-inspector"
+	gem "cucumber-rails"
+  gem "cucumber"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viiguruce::Application.config.secret_token = 'abd2795b3184391f3e7bfef9ca6e44809facf5ca117092aaeae1a14a997c1dd2aed8ec3b55f27af2e001b2beb1e3414b29d464bc9bff848c8750cb2b0076e3bc'
