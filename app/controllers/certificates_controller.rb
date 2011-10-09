f66ae2278ef1ad35433667896accb21454549a2c
Gesiel Rios <gesielrios@gmail.com>
2011-10-09 01:05:02 -0300

    Ajustes no css da aplicacao

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -1,6 +1,5 @@
 #coding: utf-8
 class CertificatesController < ApplicationController
-  
   def create
 		render :text => Certificate.generate(params[:certificate][:name]).render, :content_type => 'application/pdf'
   end
